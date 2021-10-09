ce114c8d137168ac046c0d3c59ad2c950a93caae
Andy Lau <landywl4@gmail.com>
2021-10-08 22:57:51 -0700

    Added Date Logic

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ if (process.env.NODE_ENV == 'development') {
 }
 
 function init() {
+    displayTasks(true);
     document.getElementById("add-todo").addEventListener("click", addToDo);
 }
 
@@ -52,21 +53,26 @@ function addToDo() {
         document.getElementById("modal-title").textContent = text;
     }
 
+
+    //Create input boxes in modal
     function setupInputs() {
         const modal = document.getElementById("modal-content");
         const form = createInputForm(modal);
-        appendInputsToForm(["Name", "dueDate", "description"], form);
+        appendInputsToForm([["Name","text"], ["Date", "date"], ["Description", "text"]], form);
         appendSubmitToForm(form)
     }
 
-    function createInput(buttonName) {
+    function createInput(buttonName, type) {
         const input = document.createElement("input");
-        input.type = "text";
+        const label = document.createElement("label");
+        input.type = type;
         input.required = true;
-        input.placeholder = buttonName;
         input.id = buttonName.toLowerCase();
-        input.className = "modal-input"
-        return input;
+        input.className = "modal-input";
+        label.setAttribute("for", buttonName.toLowerCase());
+        let capitalizedLetter = buttonName.charAt(0).toUpperCase();
+        label.textContent = buttonName.replace(buttonName.charAt(0), capitalizedLetter);
+        return [label, input];
     }
 
     function createInputForm(modal) {
@@ -76,26 +82,90 @@ function addToDo() {
         return inputsContainer;
     }
 
-    function appendInputsToForm(buttonList, inputsContainer) {
-        buttonList.forEach(buttonName => inputsContainer.appendChild(createInput(buttonName)));
+    function appendInputsToForm(inputList, inputsContainer) {
+        inputList.forEach(button => {
+            let inputName = button[0];
+            let type = button[1];
+            const inputAndLabel = createInput(inputName, type);
+            const label = inputAndLabel[0];
+            const input = inputAndLabel[1];
+            inputsContainer.appendChild(label);
+            inputsContainer.appendChild(input);
+        });
     }
 
     function appendSubmitToForm(form) {
         const submit = document.createElement("input");
         submit.type = "submit";
-        submit.addEventListener("click", function(e) {console.log(e)})
+        submit.addEventListener("click", createToDo);
         form.appendChild(submit);
+    }
+
+    function createToDo(e) {
+        e.preventDefault();
+        let [name, duedate, description] = getInputValues();
+        let taskElement = todoFactory(name, duedate, description);
+        localStorage.setItem("id"+localStorage.length, JSON.stringify(taskElement));
+        displayTasks();
+        cleanInputs();
+    }
+
+    function getInputValues() {
+        let name = document.getElementById("name").value;
+        console.log(name);
+        let date = document.getElementById("date").value;
+        console.log(date);
+        let description = document.getElementById("description").value;
+        console.log(description);
+        return [name, date, description];
+    }
+
+    function cleanInputs() {
+        document.getElementById("name").value = "";
+        document.getElementById("date").value  = "";
+        document.getElementById("description").value  = "";
+    }
+}
+
+function displayTasks(bypass=false) {
+    for (let i=0; i<localStorage.length; i++) {
+        let taskElement = JSON.parse(localStorage.getItem("id"+i));
+        console.log(taskElement);
+        if (taskElement.displayed == false || bypass == true) {
+            let taskDiv = document.createElement("div");
+            taskDiv.className = "task-container";
+            let name = document.createElement("h3");
+            let dueDate = document.createElement("p");
+            let description = document.createElement("p");
+            name.textContent = taskElement.name
+            dueDate.textContent = taskElement.dueDate
+            description.textContent = taskElement.name
+            taskDiv.appendChild(name);
+            taskDiv.appendChild(dueDate);
+            taskDiv.appendChild(description);
+            let currentDate = new Date().toISOString().split('T')[0];
+            let taskDate = taskElement.dueDate;
+            const msToSeconds = (1000*60*60*24);
+            let currentTimeDifference = (new Date(currentDate) - new Date(taskDate))/ msToSeconds;
+            (currentTimeDifference<-3) ? document.getElementById("priority-one").appendChild(taskDiv) :
+            (currentTimeDifference<5) ? document.getElementById("priority-two").appendChild(taskDiv) :
+            document.getElementById("priority-three").appendChild(taskDiv)
+            taskElement.displayed = true;
+            localStorage.removeItem("id"+i);
+            localStorage.setItem("id"+i, JSON.stringify(taskElement));
+        }
     }
 }
 
 const todoFactory = function(name, dueDate, description) {
-    this.name = name;
-    this.dateCreated = new Date (new Date().getTime()).toString();
-    this.dueDate = dueDate;
-    this.description = description;
+    let task = {};
+    task.name = name;
+    task.dueDate = dueDate;
+    task.description = description;
+    task.displayed = false;
+    task.createdDate = new Date(Date.now()).toString();
 
-    return this;
+    return task;
 }
 
 init();
-
